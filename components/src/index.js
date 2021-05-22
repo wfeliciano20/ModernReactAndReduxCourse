6106ce4b18efbfb9da0e018aa1709a775040e69d
wafeliciano20 <firstwillow2@gmail.com>
2021-05-21 20:16:04 -0400

    added props and AprovalCard component

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from "faker";
+import AprovalCad from "./AprovalCad";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:54 PM" 
-                picture={faker.image.avatar()} 
-                content="Awsome  blog" 
-            />
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 11:40 AM" 
-                picture={faker.image.avatar()} 
-                content="Love your blog" 
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 8:00 PM" 
-                picture={faker.image.avatar()} 
-                content="Amazing blog" 
-            />
+            <AprovalCad>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:54 PM" 
+                    picture={faker.image.avatar()} 
+                    content="Awsome  blog" 
+                />
+            </AprovalCad>
+            <AprovalCad>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 11:40 AM" 
+                    picture={faker.image.avatar()} 
+                    content="Love your blog" 
+                />
+            </AprovalCad>
+            <AprovalCad>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 8:00 PM" 
+                    picture={faker.image.avatar()} 
+                    content="Amazing blog" 
+                />
+            </AprovalCad>
         </div>
     );
 }
