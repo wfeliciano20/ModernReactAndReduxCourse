3cdb2fcfff0d4f18bdacbd6a3bd5220dc9f6871b
wafeliciano20 <firstwillow2@gmail.com>
2021-05-22 18:01:57 -0400

    refactor app to be a class and added state

--- ClassComponents/seasons/src/index.js
+++ ClassComponents/seasons/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div>Hi there</div>
-    );
-};
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            lattitude: null
+        };
+        window.navigator.geolocation.getCurrentPosition(
+        position => {
+            this.setState({ lattitude: position.coords.latitude});
+        },
+        err => console.log(err)
+        );
+
+    }
+
+    render() {
+        
+        return (
+            <div>Lattitude: {this.state.lattitude}</div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
