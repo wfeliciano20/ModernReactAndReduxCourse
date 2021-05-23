cdaef93f2f095c3ef07ff1269535f4c40692d73f
wafeliciano20 <firstwillow2@gmail.com>
2021-05-22 22:44:20 -0400

    Created a Spinner component

--- ClassComponents/seasons/src/index.js
+++ ClassComponents/seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component{
     state = { lat:null, errorMessage:''};
@@ -37,7 +38,7 @@ class App extends React.Component{
             );
         }
         else{
-            return <div>Loading!</div>
+            return <Spinner />
         } 
     };
 }
