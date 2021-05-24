99d58fe9bbb170d2033e3ca76cbeb4de915d9529
wafeliciano20 <firstwillow2@gmail.com>
2021-05-23 23:25:19 -0400

    made SearchBar a controlled component and prevented the form default behavior

--- ClassComponents/pics/src/components/SearchBar.js
+++ ClassComponents/pics/src/components/SearchBar.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+    state = { term: ''};
 
     onInputChange(event){
         const { value } = event.target
-        console.log('Input change: ' + value);
+        this.setState({ term: value});
+    }
+
+    onFormSubmit(event){
+        event.preventDefault();
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Search Bar</label>
                             <input 
                             type="text" 
                             onChange={this.onInputChange} 
+                            value={this.state.term}
                             />
                     </div>
                 </form>
