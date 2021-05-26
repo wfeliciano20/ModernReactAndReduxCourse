cae2063579cb3ce546aa8589ed1c9d13f612f5e3
wafeliciano20 <firstwillow2@gmail.com>
2021-05-25 22:29:21 -0400

    implemented SearchBar component and functionality

--- ClassComponents/youtube-videos/src/components/App.js
+++ ClassComponents/youtube-videos/src/components/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 export default class App extends Component {
+    state = {
+        videps:[]
+    };
+
+    onSearchSubmit = (term) => {
+        console.log(term);
+    }
+    
     render() {
         return (
-            <div>
-                <h1>Test</h1>
+            <div className="ui container" style={{marginTop:'10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
             </div>
         )
     }
