d819f8f69b281d3c3e48b04718afda38b87b3362
wafeliciano20 <firstwillow2@gmail.com>
2021-05-25 22:03:10 -0400

    initial files

---
+++ ClassComponents/youtube-videos/src/index.js
@@ -0,0 +1,9 @@
+import React, { createRef } from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
