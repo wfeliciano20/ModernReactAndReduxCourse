1da9a72384774600cb5c297695f37cfdc81a06da
wafeliciano20 <firstwillow2@gmail.com>
2021-05-27 15:50:41 -0400

    created the VideoList and VideoItem Components and Link them to the correct files

--- ClassComponents/youtube-videos/src/components/App.js
+++ ClassComponents/youtube-videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 export default class App extends Component {
     state = {
         videos:[]
@@ -26,6 +27,7 @@ export default class App extends Component {
         return (
             <div className="ui container" style={{marginTop:'10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <VideoList videos={this.state.videos} />
             </div>
         )
     }
---
+++ ClassComponents/youtube-videos/src/components/VideoItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class VideoItem extends Component {
+    render() {
+        return <div> VideoItem</div>;
+    }
+}
