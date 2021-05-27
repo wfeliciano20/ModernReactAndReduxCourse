a82f7936147ee59c75f1c32c1326ae98c6fe6e1c
wafeliciano20 <firstwillow2@gmail.com>
2021-05-27 18:11:32 -0400

    added custom CSS to VideoItem

--- ClassComponents/youtube-videos/src/components/VideoItem.js
+++ ClassComponents/youtube-videos/src/components/VideoItem.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./VideoItem";
 
 const VideoItem = (props) => {
     const { title, thumbnails } = props.vid.snippet;
     return (
-        <div className="item">
+        <div className="video-item item">
             <img className="ui image" alt={title} src={thumbnails.medium.url} />
             <div className="content">
                 <div className="header">{title}</div>
