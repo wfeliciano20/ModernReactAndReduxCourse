40994d4878771caad15ce63eebe1f37b93cef67c
wafeliciano20 <firstwillow2@gmail.com>
2021-05-28 19:57:13 -0400

    Made the Accordion functional with useState Hook

--- Hooks/widgets/src/components/App.js
+++ Hooks/widgets/src/components/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Accordion from "./Accordion";
 
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a front end javascript framework",
+    },
+    {
+        title: "Why use React?",
+        content: "React is a favorite among engineers",
+    },
+    {
+        title: "How do you use React?",
+        content: "You use React by creating components",
+    },
+];
+
 const App = () => {
     return (
         <div className="ui container">
-            <Accordion />
+            <Accordion items={items} />
         </div>
     );
 };
