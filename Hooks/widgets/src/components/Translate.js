130d6b116c2b124c893191cca8a98ce1c934820d
wafeliciano20 <firstwillow2@gmail.com>
2021-06-03 22:01:44 -0400

    added an input field

--- Hooks/widgets/src/components/Translate.js
+++ Hooks/widgets/src/components/Translate.js
@@ -22,8 +22,19 @@ const options = [
 
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState("");
     return (
         <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                    />
+                </div>
+            </div>
+
             <Dropdown
                 label="Select a language"
                 options={options}
