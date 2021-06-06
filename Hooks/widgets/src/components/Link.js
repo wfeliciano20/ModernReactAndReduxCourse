9b51c84603bcf229ee81abf7e68ca8f456781b83
wafeliciano20 <firstwillow2@gmail.com>
2021-06-05 23:03:56 -0400

    added a conditional to listen for command or ctrl keys to add a new tab and do brwoser normal behaviour

--- Hooks/widgets/src/components/Link.js
+++ Hooks/widgets/src/components/Link.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Link = ({ href, className, children }) => {
     const onClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+
         event.preventDefault();
         window.history.pushState({}, "", href);
         const navEvent = new PopStateEvent("popstate");
