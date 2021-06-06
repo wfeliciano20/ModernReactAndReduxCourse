297a21a7112df7cb64693ac6d1e22090702a382c
wafeliciano20 <firstwillow2@gmail.com>
2021-06-05 22:01:35 -0400

    created Route component and in App use the Route component to show the different widgets

--- Hooks/widgets/src/components/App.js
+++ Hooks/widgets/src/components/App.js
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
-// import Accordion from "./Accordion";
-// import Search from "./Search";
-//import Dropdown from "./Dropdown";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
 import Translate from "./Translate";
+import Route from "./Route";
 
-// const items = [
-//     {
-//         title: "What is React?",
-//         content: "React is a front end javascript framework",
-//     },
-//     {
-//         title: "Why use React?",
-//         content: "React is a favorite among engineers",
-//     },
-//     {
-//         title: "How do you use React?",
-//         content: "You use React by creating components",
-//     },
-// ];
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a front end javascript framework",
+    },
+    {
+        title: "Why use React?",
+        content: "React is a favorite among engineers",
+    },
+    {
+        title: "How do you use React?",
+        content: "You use React by creating components",
+    },
+];
 
-// const options = [
-//     {
-//         label: "The color red",
-//         value: "red",
-//     },
-//     {
-//         label: "The color green",
-//         value: "green",
-//     },
-//     {
-//         label: "A shade of blue",
-//         value: "blue",
-//     },
-// ];
+const options = [
+    {
+        label: "The color red",
+        value: "red",
+    },
+    {
+        label: "The color green",
+        value: "green",
+    },
+    {
+        label: "A shade of blue",
+        value: "blue",
+    },
+];
 
 const App = () => {
-    // const [selected, setSelected] = useState(options[0]);
+    const [selected, setSelected] = useState(options[0]);
     // const [showDropdown, setShowDropdown] = useState(true);
     return (
         <div className="ui container">
@@ -52,7 +53,24 @@ const App = () => {
                     options={options}
                 />
             ) : null} */}
-            <Translate />
+            {/* <Translate /> */}
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a Color"
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    options={options}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
