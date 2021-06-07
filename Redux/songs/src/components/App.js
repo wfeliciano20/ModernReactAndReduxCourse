696be7a6acfae7a1ee6bf66dd26f32a5830e1c8e
wafeliciano20 <firstwillow2@gmail.com>
2021-06-07 12:22:08 -0400

    initial files

---
+++ Redux/songs/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <div>
+                <h1>Songs</h1>
+            </div>
+        </div>
+    );
+};
+
+export default App;
