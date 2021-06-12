9937642e03565ffa980d838e9a4ef3bc042974fb
wafeliciano20 <firstwillow2@gmail.com>
2021-06-11 21:05:31 -0400

    created the fetchUser action creator fixed

--- Redux/blog/src/actions/index.js
+++ Redux/blog/src/actions/index.js
@@ -7,7 +7,7 @@ export const fetchPosts = () => {
     };
 };
 
-export const fetchUsers = (id) => {
+export const fetchUser = (id) => {
     return async (dispatch) => {
         const response = await jsonPlaceholder.get(`/posts/${id}`);
         dispatch({ type: "FETCH_USER", payload: response.data });
