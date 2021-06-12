e724dd6b3f3c7d6eec2f83833f47a2cd8bfcd128
wafeliciano20 <firstwillow2@gmail.com>
2021-06-11 23:54:18 -0400

    memoized a private _fetchUser function to only make 1 request with unique id

--- Redux/blog/src/actions/index.js
+++ Redux/blog/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => {
     return async (dispatch) => {
@@ -7,9 +8,11 @@ export const fetchPosts = () => {
     };
 };
 
-export const fetchUser = (id) => {
-    return async (dispatch) => {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
-        dispatch({ type: "FETCH_USER", payload: response.data });
-    };
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: "FETCH_USER", payload: response.data });
+});
