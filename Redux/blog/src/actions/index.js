8512edbad4331f8fa1f705c9beb28d13368c885e
wafeliciano20 <firstwillow2@gmail.com>
2021-06-12 20:42:11 -0400

    created a new action creator fetchPostsAndUsers  which uses the fetchPosts and fetchUser

--- Redux/blog/src/actions/index.js
+++ Redux/blog/src/actions/index.js
@@ -1,5 +1,12 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
 import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState.posts, "userid"));
+    userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => {
     return async (dispatch) => {
@@ -8,11 +15,17 @@ export const fetchPosts = () => {
     };
 };
 
-export const fetchUser = (id) => (dispatch) => {
-    _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// memoize version with lodash
+
+// export const fetchUser = (id) => async (dispatch) => {
+//     _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+// });
